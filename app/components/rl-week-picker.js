b5382f7f8b34789eb4f6a52ea98e0f79a4639506
R.S. Schermer <roland0507@gmail.com>
2014-11-02 12:02:08 +0100

    Demo app

--- app/components/rl-week-picker.js
+++
@@ -1,3 +0,0 @@
-import RlWeekPickerComponent from 'ember-rl-week-picker/components/rl-week-picker';
-
-export default RlWeekPickerComponent;
--- blueprints/rl-week-picker-css/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'rl-week-picker-css',
-
-  normalizeEntityName: function() {},
-
-  description: 'Generates the basic css for the rl-week-picker component.'
-};
